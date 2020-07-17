dd2ff6aecda8ecaf5691020cfa3e839294262753
BonnieNyambura <nyamburabonnie@gmail.com>
2020-07-17 14:47:38 +0300

    react with hooks

--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
-import React, {useState} from "react";
+import React from "react";
 import ReactDOM from "react-dom";
+import { TaskApp } from "./components/taskApp";
 
-
-
-var TaskApp=()=>{
-  var [task,setTasks]=useState([]);
-  var [title, setTitle]= useState("");
-  var [body, setBody]=useState("");
-
-  return(
-    <div>
-      <h1>Tasks</h1>
-      <p>Add Taks</p>
-      <form>
-        <input value={title} onChange={(evt)=> setTitle(evt.target.value)}></input>;
-        <textarea value={body} onChange={(evt)=> setBody}
-      </form>
-    </div>
-  )
-}
+ReactDOM.render(<TaskApp />, document.getElementById("root"));
